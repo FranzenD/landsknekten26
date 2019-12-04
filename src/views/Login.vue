76a146253bd78006899ea0a3510047c9413945ac
Daniel Franzén <Daniel@Daniels-MBP-2.lan>
2019-12-04 22:24:27 +0100

    Added login view

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login">
+        <p>LOGGA IN</p>
+        <input v-model="username" type="text" placeholder="Användarnamn">
+        <input v-model="password" type="password" placeholder="Lösenord">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data(){
+        return{
+            username: '',
+            password: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.login{
+    input{
+        padding: 10px;
+    }
+}
+</style>
